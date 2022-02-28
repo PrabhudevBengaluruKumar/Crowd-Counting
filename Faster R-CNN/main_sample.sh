7d35435012a8607b8b3e10676f017575a4fe5e0b
Kaushik Manjunatha <38116333+kaushikmupadhya@users.noreply.github.com>
2022-02-28 21:39:01 +0100

    Update main_sample.sh

--- Faster R-CNN/main_sample.sh
+++ Faster R-CNN/main_sample.sh
@@ -7,16 +7,18 @@
 #SBATCH --time=72:00:00           # HH-MM-SS
 #SBATCH --output /home/pbenga2s/RnD/crowd-counting-using-tensorflow/job_tf.%N.%j.out # filename for STDOUT (%N: nodename, %j: j$
 #SBATCH --error /home/pbenga2s/RnD/crowd-counting-using-tensorflow/job_tf.%N.%j.err  # filename for STDERR
+
 # to load CUDA module
 module load cuda
 
-# activate environment
+# activate the environment
 source ~/anaconda3/bin/activate ~/anaconda3/envs/RnD
 
 # locate to your root directory
 cd /home/pbenga2s/RnD/crowd-counting-using-tensorflow
+
 # run the script
 python main_sample.py
 
-#Find your batch status in https://wr0.wr.inf.h-brs.de/wr/stat/batch.xhtml
+#Find your batch status in "https://wr0.wr.inf.h-brs.de/wr/stat/batch.xhtml"
 
